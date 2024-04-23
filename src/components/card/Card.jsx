4cf924d3c2b991fdf6e3b2106060e57fde7339b6
Faisal <138551448+FaisalNadaf@users.noreply.github.com>
2024-04-23 22:15:07 +0530

    code

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
 
 const Card = () => {
   return (
-    <div>Card</div>
-  )
-}
+    <div className="h-60 w-60 border-2 border-black rounded-3xl p-4 mx-20 m-10">
+      <div className="flex justify-between items-center px-4 border-b-2 ">
+        <div className="text-2xl font-bold">Title</div>
+        <div>
+          <i class="fa-solid fa-trash hover:text-[red] opacity-70  hover:opacity-100"></i>
+        </div>
+      </div>
+      <div className="text-xl p-4">
+        Description : Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Ab, molestiae.
+      </div>
+      <span className="relative left-48 bottom-4 ">
+        <input type="checkbox" defaultChecked className="checkbox h-5 w-5 " />
+      </span>
+    </div>
+  );
+};
 
-export default Card
+export default Card;
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -4,9 +4,13 @@ import Card from "../../components/card/Card";
 
 const Home = () => {
   return (
-    <div className="h-screen w-screen flex justify-center items-center">
-        <AddTask/>
-        <Card/> 
+    <div className="h-screen w-screen  ">
+      <div className="flex justify-center items-end h-1/5 mb-10">
+        <AddTask />
+      </div>
+      <div className="flex flex-wrap items-center justify-center px-20">
+       
+      </div>
     </div>
   );
 };
